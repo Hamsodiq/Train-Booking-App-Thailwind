2881102cf5651a500d78a948f83f9f985fa4be6a
Hammed Omotola <hamsodiq203@gmail.com>
2026-01-10 12:36:03 +0100

    linking the pages together

--- js/index.js
+++ js/index.js
@@ -121,7 +121,7 @@ window.onload = function () {
     document.getElementById("signupForm").reset();
 
     setTimeout(() => {
-      window.location.href = "./html/login.html";
+      window.location.href = "/TRAIN-BOOKING-APP-THAILWIND/html/login.html";
     }, 2000);
   }
 };
